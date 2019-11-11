af745912908e1857835a0ee7ce85fe6fc52fadc7
Carlos G Lopez <carlos@carlosglopez.com>
2019-11-11 12:15:43 -0500

    first commit

---
+++ precache-manifest.c84576b7964495a0841c0ea7bdce2a2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a409e71ca5a4e335478681fc2cdc5e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e33140b1646f58748465",
+    "url": "/static/css/2.d0bea89d.chunk.css"
+  },
+  {
+    "revision": "e33140b1646f58748465",
+    "url": "/static/js/2.0893f8e5.chunk.js"
+  },
+  {
+    "revision": "59e18962b98210cd9ca1",
+    "url": "/static/js/main.54f415f8.chunk.js"
+  },
+  {
+    "revision": "30fa3c7c18b5dce4910d",
+    "url": "/static/js/runtime-main.0c39a938.js"
+  }
+]);
